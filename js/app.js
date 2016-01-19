7c0979ab6c7a5a53c2d9a2f0ab5b8aa31341c207
zach georgii <zgeorgii@gmail.com>
2016-01-19 12:00:06 -0800

    added js

---
+++ js/app.js
@@ -0,0 +1,25 @@
+var images = ['img/', 'img/', 'img/', 'img/', 'img/', 'img/', 'img/', 'img/', 'img/', 'img/', 'img/', 'img/', 'img/', 'img/', 'img/'];
+
+var usedImgLeft = {};
+var imgLeftCount = 0;
+
+function displayImgLeft(){
+  var img1 = Math.floor(Math.random())
+}
+
+var usedImgCenter = {};
+var imgCenterCount = 0;
+
+function displayImgCenter(){
+ var img2 = Math.floor(Math.random())
+}
+
+var usedImgRight = {};
+var imgRightCenter = 0;
+
+function displayImgRight(){
+  var img3 = Math.floor(Math.random())
+}
+
+//img labels and click amounts
+var allProducts = [];
